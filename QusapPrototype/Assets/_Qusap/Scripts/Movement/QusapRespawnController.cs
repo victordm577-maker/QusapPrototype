7629f34c20f64fdd000b0f1e76f3ada1256b664a
victordm577-maker <victordm577@gmail.com>
2026-07-31 17:57:52 -0500

    feat: add movement lab respawn

---
+++ QusapPrototype/Assets/_Qusap/Scripts/Movement/QusapRespawnController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+namespace Qusap
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class QusapRespawnController : MonoBehaviour
+    {
+        [SerializeField] private float fallLimitY = -8f;
+        [SerializeField] private Transform optionalRespawnPoint;
+
+        private Rigidbody rb;
+        private Vector3 initialPosition;
+        private Quaternion initialRotation;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+            initialPosition = rb.position;
+            initialRotation = rb.rotation;
+        }
+
+        private void FixedUpdate()
+        {
+            if (rb.position.y < fallLimitY)
+            {
+                Respawn();
+            }
+        }
+
+        public void Respawn()
+        {
+            Vector3 respawnPosition = optionalRespawnPoint != null
+                ? optionalRespawnPoint.position
+                : initialPosition;
+            Quaternion respawnRotation = optionalRespawnPoint != null
+                ? optionalRespawnPoint.rotation
+                : initialRotation;
+
+            rb.position = respawnPosition;
+            rb.rotation = respawnRotation;
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.WakeUp();
+        }
+    }
+}
